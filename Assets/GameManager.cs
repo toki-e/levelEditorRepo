e314556360fb41f73d14acdaf163abd9a2553a55
Unknown <kaa516@nyu.edu>
2018-03-02 12:34:03 -0500

    initial project commit with level editor, walls, door and player

---
+++ Assets/GameManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+using System.IO; //need system io for level editing
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance = null; 
+
+	public GameObject playerPrefab;
+	public GameObject wallPrefab;
+	public GameObject doorPrefab;
+
+	public float tileWidth = 2f;
+	public float tileHeight = 3f;
+
+	public string levelString = "xooopx";
+	public string levelFile = "level1.txt";
+
+	// Use this for initialization
+	void Start () {
+
+		string levelString = File.ReadAllText (Application.dataPath + Path.DirectorySeparatorChar + levelFile);
+
+		string[] levelLines = levelString.Split ('\n');
+		int width = 0;
+
+		//makes sure unity can recognize the rows and columns in the txt file
+		for (int row = 0; row < levelLines.Length; row++) {
+			string currentLine = levelLines [row];
+			width = currentLine.Length;
+
+			for (int col = 0; col < currentLine.Length; col++) {
+				char currentChar = currentLine [col];
+				if (currentChar == 'x') {
+					GameObject wallObj = Instantiate (wallPrefab);
+					wallObj.transform.position = new Vector3 (col * tileWidth, -row * tileHeight, 0);
+				} else if (currentChar == 'p') {
+					GameObject playerObj = Instantiate (playerPrefab);
+					playerObj.transform.position = new Vector3 (col * tileWidth, -row * tileHeight, 0);
+				} else if (currentChar == 'd') {
+					if(Random.value <= 0.5f){
+						GameObject enemyObj = Instantiate (doorPrefab);
+						enemyObj.transform.position = new Vector3 (col * tileWidth, -row * tileHeight, 0);
+					}
+				}
+			}
+		}
+
+		//makes sure unity reads the text file in the right order
+		float myX = -(width * tileWidth)/2f + tileWidth/2f;
+		float myY = (levelLines.Length*tileHeight)/2f - tileHeight/2f;
+		transform.position = new Vector3 (myX, myY, 0);
+
+	if (instance == null) {
+		DontDestroyOnLoad(gameObject);
+		instance = this;
+	}
+	else {
+		Destroy(gameObject);
+	}
+}
+
+// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public KeyCode upKey = KeyCode.W;
+	public KeyCode rightKey = KeyCode.D;
+	public KeyCode downKey = KeyCode.S;
+	public KeyCode leftKey = KeyCode.A;
+	public float moveSpeed = 10f;
+
+
+	Rigidbody2D _body;
+
+
+	// Use this for initialization
+	void Start () {
+		_body = GetComponent<Rigidbody2D>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		Vector2 newVelocity = new Vector2(0, 0);
+		if (Input.GetKey(upKey)) {
+			newVelocity.y += moveSpeed;
+		}
+		if (Input.GetKey(rightKey)) {
+			newVelocity.x += moveSpeed;
+			GetComponent<SpriteRenderer> ().flipX = true;
+		}
+		if (Input.GetKey(downKey)) {
+			newVelocity.y -= moveSpeed;
+		}
+		if (Input.GetKey(leftKey)) {
+			newVelocity.x -= moveSpeed;
+			GetComponent<SpriteRenderer> ().flipX = false;
+		}
+
+		newVelocity = newVelocity.normalized*moveSpeed;
+
+		_body.velocity = newVelocity;
+	}
+}
